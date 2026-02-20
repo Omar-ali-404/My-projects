ba6ee4134139c60d4cf97ad0277a3af069388c0b
Omar Ali <oali95333@gmail.com>
2026-02-20 06:23:40 +0200

    Delete car.py

--- car.py
+++
@@ -1,6 +0,0 @@
-age = int(input("how old are you?\n:"))
-licence = input("do you have driver licence(yes , no )?\n:")
-if age >=18 and licence == "yes":
-    print("you can drive acar")
-elif age<18 or licence != "yes":
-    print ("sorry , you cant drive acar")
